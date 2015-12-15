03d98eb2eaa625feb9a58c27ca4171b256034504
mndivya <nagadivya.m@gmail.com>
2015-12-15 11:37:38 -0500

    created java library

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.gradle.jokes.example.JokesClass;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -40,7 +42,10 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        JokesClass joke = new JokesClass();
+
+        Toast.makeText(this, joke.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ FinalProject/jokeslibrary/src/main/java/com/gradle/jokes/example/JokesClass.java
@@ -0,0 +1,7 @@
+package com.gradle.jokes.example;
+
+public class JokesClass {
+    public String getJoke(){
+        return "This is totally a funny joke";
+    }
+}
